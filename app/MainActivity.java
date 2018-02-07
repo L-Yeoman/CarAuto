f57c0274857a8aa1977161d444b97e1dc6474b68
MrSuncm <z332315013@qq.com>
2018-02-07 15:40:48 +0800

    增加主类

---
+++ app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sss.lottery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        setContentView(R.layout.activity_main);
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+    }
+}
